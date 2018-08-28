6470839c4284afbbe5637f98a7c6e6d14e0e4b2c
Nathaniel Makinde <nathmankind@gmail.com>
2018-08-28 03:22:37 +0100

    pages modified

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$('.navbar li').on('click', function(){
-  alert('hey navbar');
-  $('.navbar').find('.active').removeClass('active');
-  $(this).parent().addClass('active');
-});
+// $('.navbar li').on('click', function(){
+//   alert('hey navbar');
+//   $('.navbar').find('.active').removeClass('active');
+//   $(this).parent().addClass('active');
+// });
 
 
 $('.counter').each(function() {
@@ -31,3 +31,22 @@ $('.counter').each(function() {
   
   });
 
+  // form validate
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
